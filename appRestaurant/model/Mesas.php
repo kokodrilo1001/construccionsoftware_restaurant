50880386735c60b778448bffa9f067af6174b97b
Mayita6 <90015301+Mayita6@users.noreply.github.com>
2021-09-07 23:56:08 -0500

    Entidad de mesas
    entidad

---
+++ appRestaurant/model/Mesas.php
@@ -0,0 +1,125 @@
+<?php
+Class Empleado{
+    /* @var int */
+    private $codigo;
+    /* @var string */
+    private $apellidos;
+    private $nombres;
+	private $nombreCompleto;
+    /* @var usuario */
+    private $usuario;
+
+    public function __construct($codigo){
+        $this->codigo=$codigo;
+    }
+
+
+ 
+    /**
+     * Get the value of codigo
+     */ 
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    /**
+     * Set the value of codigo
+     *
+     * @return  self
+     */ 
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+
+
+
+
+
+    /**
+     * Get the value of apellidos
+     */ 
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set the value of apellidos
+     *
+     * @return  self
+     */ 
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombres
+     */ 
+    public function getNombres()
+    {
+        return $this->nombres;
+    }
+
+    /**
+     * Set the value of nombres
+     *
+     * @return  self
+     */ 
+    public function setNombres($nombres)
+    {
+        $this->nombres = $nombres;
+
+        return $this;
+    }
+
+	/**
+	 * Get the value of nombreCompleto
+	 */ 
+	public function getNombreCompleto()
+	{
+		return $this->nombreCompleto;
+	}
+
+	/**
+	 * Set the value of nombreCompleto
+	 *
+	 * @return  self
+	 */ 
+	public function setNombreCompleto($nombreCompleto)
+	{
+		$this->nombreCompleto = $nombreCompleto;
+
+		return $this;
+	}
+
+    
+
+    /**
+     * Get the value of usuario
+     */ 
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * Set the value of usuario
+     *
+     * @return  self
+     */ 
+    public function setUsuario($usuario)
+    {
+        $this->usuario = $usuario;
+
+        return $this;
+    }
+}
+?>
